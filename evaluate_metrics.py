e5242dc218208435f2b67b9da62214a96e3e4fad
jpulmano <jeremypulmano@gmail.com>
2021-04-25 23:08:25 -0400

    Update evaluate_metrics.py

--- evaluate_metrics.py
+++ evaluate_metrics.py
@@ -7,10 +7,6 @@ import wandb
 
 wandb.init(project="LatentPromptAnalysis-Metrics")
 
-wandb.log({
-	'time_vessels_path': time_vessels_path
-})
-
 if len(sys.argv) < 4:
 	print('use like: evaluate_metrics.py <path/to/time_vessels> <num_test> <predictions_file>')
 	exit()
@@ -19,6 +15,10 @@ time_vessels_path = sys.argv[1]
 num_tests = int(sys.argv[2])
 predictions_file = sys.argv[3]
 
+wandb.log({
+	'time_vessels_path': time_vessels_path,
+	'num_tests': num_tests
+})
 
 f1_positives = []
 f1_negatives = []
@@ -37,21 +37,27 @@ for test_num in range(num_tests):
 	f1_negatives.append(f1_score(y_true=labels_true, y_pred=labels_pred, pos_label=0))
 	accuracies.append(accuracy_score(y_true=labels_true, y_pred=labels_pred))
 
-print('F1 pos: {} ({}) : {}'.format(mean(f1_positives), stdev(f1_positives), median(f1_positives)))
-print('F1 neg: {} ({}) : {}'.format(mean(f1_negatives), stdev(f1_negatives), median(f1_negatives)))
-print('Accuracy: {} ({}) : {}'.format(mean(accuracies), stdev(accuracies), median(accuracies)))
 
-wandb.log({
-	'F1_pos_mean': mean(f1_positives),
-	'F1_pos_stddev': stdev(f1_positives),
-	'F1_pos_median': median(f1_positives),
-	'F1_neg_mean': mean(f1_negatives),
-	'F1_neg_stddev': stdev(f1_negatives),
-	'F1_neg_median': median(ff1_negatives),
-	'Acc_mean': mean(accuracies),
-	'Acc_stddev': stdev(accuracies),
-	'Acc_median': median(accuracies),
-})
+print('f1_positives: ', f1_positives)
+print('f1_negatives: ', f1_negatives)
+print('accuracies: ', accuracies)
+
+if len(f1_positives) > 1:
+	print('F1 pos: {} ({}) : {}'.format(mean(f1_positives), stdev(f1_positives), median(f1_positives)))
+	print('F1 neg: {} ({}) : {}'.format(mean(f1_negatives), stdev(f1_negatives), median(f1_negatives)))
+	print('Accuracy: {} ({}) : {}'.format(mean(accuracies), stdev(accuracies), median(accuracies)))
+	
+	wandb.log({
+		'F1_pos_mean': mean(f1_positives),
+		'F1_pos_stddev': stdev(f1_positives),
+		'F1_pos_median': median(f1_positives),
+		'F1_neg_mean': mean(f1_negatives),
+		'F1_neg_stddev': stdev(f1_negatives),
+		'F1_neg_median': median(ff1_negatives),
+		'Acc_mean': mean(accuracies),
+		'Acc_stddev': stdev(accuracies),
+		'Acc_median': median(accuracies),
+	})
 
 
 
